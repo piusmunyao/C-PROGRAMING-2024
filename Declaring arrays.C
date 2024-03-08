922be9bd32c1aaaf977a7c291bbec2bd8941a2d3
piusmunyao <158205901+piusmunyao@users.noreply.github.com>
2024-03-08 16:03:51 +0300

    Add files via upload
    I used C function to declare, initialize and print a {3} {3} {2}.

---
+++ Declaring arrays.C
@@ -0,0 +1,6 @@
+//Declaring,initializing and printing a{3}{3}{2};
+#include <stdio.h>
+int main() {
+int a[3][3][2] = {{{1, 2}, {3, 4}, {5, 6}}, {{7, 8}, {9, 10}, {11, 12}}, {{13, 14}, {15, 16}, {17, 18}}};
+    return 0;
+}
